8983610527916799ce8764675f0625fdb49effa1
JulKramer3G <juliuskramer00@gmail.com>
2023-07-24 09:00:27 +0200

    file and clipboard

--- run-speedtest.sh
+++ run-speedtest.sh
@@ -15,7 +15,7 @@ do
 
     # check if /RUN File exists or clipboard set to RUN, and time is past 7:30
     # if [[ -f "/RUN" ]] && [[ $(date +%H:%M) > "07:30" ]]
-    if [[ "$clipboard" == "RUN" ]] && [[ $(date +%H:%M) > "07:30" ]]
+    if [[ "$clipboard" == "RUN" || -f "/RUN" ]] && [[ $(date +%H:%M) > "07:30" ]]
     then
         echo "Tring to start speedtest..."
         
